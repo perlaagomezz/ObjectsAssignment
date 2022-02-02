0b736f58a0ad8d4e32563e1265350f87562a1f52
perlaagomezz <gomezperla879@gmail.com>
2022-02-02 09:08:41 -0800

    added somethigs

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict'
+
+console.log("hello there its perlaaa");
+
+document.write("<h1> Object Assignment </h1>");
+
+const cat = {
+ breed: "pixie cat",
+  age: "1 year old ",
+  color: "greyish",
+}
+function()
+document.write( <h2>This is my cat let me tell you about him.</h2>)
+return("My cat is a " + this.breed + " it is a rare cat.")
+}
+console.log("my spaceship is", spaceship.color);
+
+console.log(spaceship.flying());
+
+document.write("<h2>", spaceship.flying(), "</h2>")
+document.write("<h2> My spaceship is ", spaceship.color, " and has a ", spaceship.room, " for style and comfomort! <h2>")
+
+function Cat (breed, age, color){
+  this.breed = breed;
+  this.age = age;
+  this.color = color;
+}
+
+Cat.prototype.eyecolor = "blue"
+
+const brothersCat = new Cat  ('savannah cat', '3', 'black');
+
+const dadsCat = new Cat('bengal cat', '2', 'cheata print');
+
+const sistersCat = new Cat ('ragdoll', '1', 'white');
+
+console.log(brothersCat);
+console.log(dadsCat);
+console.log(sistersCat, cat.prototype);
